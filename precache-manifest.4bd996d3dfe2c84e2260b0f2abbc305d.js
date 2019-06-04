3721526597f7bea0ed67ce8ca69d6fcb77ef07be
Melisa Quesada <melisa.quc@gmail.com>
2019-06-04 14:25:59 -0500

    Updates

---
+++ precache-manifest.4bd996d3dfe2c84e2260b0f2abbc305d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d5f977db35da3753064ba53b7162a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2859fb7758eb6be49678",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "38a6dac26eefa7a8efc1",
+    "url": "/static/js/2.e7fe9646.chunk.js"
+  },
+  {
+    "revision": "2859fb7758eb6be49678",
+    "url": "/static/js/main.b864090a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
