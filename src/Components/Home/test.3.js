d1817eaccc43d05d002fc46120c95bdf20253db6
Melisa Quesada <melisa.quc@gmail.com>
2019-06-12 17:10:23 -0500

    limited movement to X axis

--- src/Components/Home/test.3.js
+++ src/Components/Home/test.3.js
@@ -79,7 +79,9 @@ export default class ThreeScene extends Component {
         // this.cube.rotation.y += 0.01;
 
         this.target.x = (this.mouse.x) * 0.003;
-        this.target.y = (this.mouse.y) * 0.003;
+
+        //This line makes the camera rotate in the Y axis
+        // this.target.y = (this.mouse.y) * 0.003;
 
         this.cube.rotation.x += 0.05 * (this.target.y - this.cube.rotation.x);
         this.cube.rotation.y += 0.05 * (this.target.x - this.cube.rotation.y);
