0a746e4b48e1e661a88844eab0c37496ec6d8bd4
Melisa Quesada <melisa.quc@gmail.com>
2019-06-27 10:18:45 -0500

    modifies gitignore

---
+++ build/precache-manifest.5fbdd12eea6025dc498d09fd5c5c55fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a8bf76e635064bfaedaa6cafa535ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2067ec770880fa85f471",
+    "url": "/static/css/2.1cb62e95.chunk.css"
+  },
+  {
+    "revision": "1bc6c0afebc26cbcc7ec",
+    "url": "/static/css/main.d615170d.chunk.css"
+  },
+  {
+    "revision": "2067ec770880fa85f471",
+    "url": "/static/js/2.e1c33d9a.chunk.js"
+  },
+  {
+    "revision": "1bc6c0afebc26cbcc7ec",
+    "url": "/static/js/main.3bc4339d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "395b67e6199f78e84ec45901e7684299",
+    "url": "/static/media/contact-text.395b67e6.png"
+  }
+]);
