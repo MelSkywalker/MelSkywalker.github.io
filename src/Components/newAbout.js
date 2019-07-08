cbb388acce2b623559d0f19719dca9c06e2a5115
Melisa Quesada <melisa.quc@gmail.com>
2019-07-08 12:20:15 -0500

    adds CNAME

--- src/Components/newAbout.js
+++ src/Components/newAbout.js
@@ -4,6 +4,14 @@ import itSkills from './About/itskills.json';
 import mgSkills from './About/mgskills.json';
 import softSkills from './About/softskills.json';
 import Resume from './About/cv.js';
+import {Javascript} from 'styled-icons/boxicons-logos/Javascript';
+import {Html5} from 'styled-icons/boxicons-logos/Html5';
+import {Css3} from 'styled-icons/boxicons-logos/Css3';
+import {ReactLogo} from 'styled-icons/boxicons-logos/ReactLogo';
+import {Nodejs} from 'styled-icons/boxicons-logos/Nodejs';
+import {Git} from 'styled-icons/boxicons-logos/Git';
+import {Github} from 'styled-icons/boxicons-logos/Github';
+// import {FileJson} from ' styled-icons/boxicons-solid/FileJson';
 
 const MainContainer = styled.div`
     display: flex;
@@ -17,8 +25,8 @@ const MainContainer = styled.div`
 const Container = styled.div`
     margin: 0;
     padding: 0;
-    width: 20%;
-    height: 50%;
+    width: 80%;
+    /* height: 50%; */
     padding: 1em;
     border-radius: 20px;
     background-image: linear-gradient(#42f5d7, #17e5e8);
@@ -48,6 +56,11 @@ const About = () => {
         <MainContainer>
             <Resume/>
             <Container>
+                <P>My name is Melisa Quesada. I am a Frontend developer but also a Motion designer.</P>
+                <P>I describe myself as a creative problem solver.</P>
+                <Javascript/> <Html5/> <Css3/> <ReactLogo/> <Nodejs/> <Git/> <Github/>
+            </Container>
+            {/* <Container>
                 <P>Hello! My name is Melisa Quesada but you can call me Mel.</P>
                 <P>I am a Frontend Developer but also a Motion designer!</P>
                 <P>I describe myself as a creative problem solver.</P>
@@ -64,7 +77,7 @@ const About = () => {
                 <List>
                     {softSkills.map(skill => <ItemSoft key={skill.skill}>{skill.skill}</ItemSoft>)}
                 </List>
-            </Container>
+            </Container> */}
         </MainContainer>
     );
 };
